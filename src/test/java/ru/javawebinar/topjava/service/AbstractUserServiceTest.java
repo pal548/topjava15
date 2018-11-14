fa371f0a6d93a2883cd866724e3a2648d2961e42
Лукьянский П.А <affklay@gmail.com>
2018-11-15 01:36:56 +0300

    added ROLE_USER to ADMIN, enable roles matching in asserts. Jdbc tests stopped working

--- src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/AbstractUserServiceTest.java
@@ -56,8 +56,8 @@ public abstract class AbstractUserServiceTest extends AbstractServiceTest {
 
     @Test
     public void get() throws Exception {
-        User user = service.get(USER_ID);
-        assertMatch(user, USER);
+        User user = service.get(ADMIN_ID);
+        assertMatch(user, ADMIN);
     }
 
     @Test(expected = NotFoundException.class)
