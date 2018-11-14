7747b2ed85471a6b70ce4e2aa255815f0ac3496d
Лукьянский П.А <affklay@gmail.com>
2018-11-15 00:01:54 +0300

    finished controller without "action" parameter, finished localization

--- src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/JspMealController.java
@@ -4,11 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import ru.javawebinar.topjava.model.Meal;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -19,58 +19,59 @@ import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalDate;
 import static ru.javawebinar.topjava.util.DateTimeUtil.parseLocalTime;
 
 @Controller
+@RequestMapping(JspMealController.MEALS_PATH)
 public class JspMealController extends AbstractMealController {
 
-    @GetMapping("/meals")
-    public String getMeals(Model model, HttpServletRequest request) {
-        String action = request.getParameter("action");
+    static final String MEALS_PATH = "/meals";
 
-        switch (action == null ? "all" : action) {
-            case "delete":
-                int id = getId(request);
-                super.delete(id);
-                return "redirect:meals";
-            case "create":
-            case "update":
-                final Meal meal = "create".equals(action) ?
-                        new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
-                        super.get(getId(request));
-                model.addAttribute("meal", meal);
-                return "mealForm";
-            case "all":
-            default:
-                model.addAttribute("meals", super.getAll());
-                return "meals";
-        }
+    @GetMapping("")
+    public String getMeals(Model model) {
+        model.addAttribute("meals", super.getAll());
+        return "meals";
     }
 
-    @PostMapping("/meals")
-    public String postMeals(Model model, HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        String action = request.getParameter("action");
-        if (action == null) {
-            Meal meal = new Meal(
-                    LocalDateTime.parse(request.getParameter("dateTime")),
-                    request.getParameter("description"),
-                    Integer.parseInt(request.getParameter("calories")));
+    @GetMapping("/delete")
+    public String deleteMeal(HttpServletRequest request) {
+        int id = getId(request);
+        super.delete(id);
+        return "redirect:.." + MEALS_PATH;
+    }
 
-            if (request.getParameter("id").isEmpty()) {
-                super.create(meal);
-            } else {
-                super.update(meal, getId(request));
-            }
-            //response.sendRedirect("meals");
-            return "redirect:meals";
+    @GetMapping({"/create", "/update"})
+    public String createUpdateMeal(Model model, HttpServletRequest request) {
+        var path = request.getServletPath();
+        var action = path.substring(path.lastIndexOf("/")+1);
+        final var meal = "create".equals(action) ?
+                new Meal(LocalDateTime.now().truncatedTo(ChronoUnit.MINUTES), "", 1000) :
+                super.get(getId(request));
+        model.addAttribute("action", action);
+        model.addAttribute("meal", meal);
+        return "mealForm";
+    }
 
-        } else if ("filter".equals(action)) {
-            LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
-            LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
-            LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
-            LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
-            model.addAttribute("meals", super.getBetween(startDate, startTime, endDate, endTime));
-            return "meals";
+    @PostMapping({"/create", "/update"})
+    public String postMeals(HttpServletRequest request) throws ServletException, IOException {
+        Meal meal = new Meal(
+                LocalDateTime.parse(request.getParameter("dateTime")),
+                request.getParameter("description"),
+                Integer.parseInt(request.getParameter("calories")));
+
+        if (request.getParameter("id").isEmpty()) {
+            super.create(meal);
+        } else {
+            super.update(meal, getId(request));
         }
-        return "redirect:meals";
+        return "redirect:.." + MEALS_PATH;
+    }
+
+    @PostMapping("")
+    public String filterMeals(Model model, HttpServletRequest request) {
+        LocalDate startDate = parseLocalDate(request.getParameter("startDate"));
+        LocalDate endDate = parseLocalDate(request.getParameter("endDate"));
+        LocalTime startTime = parseLocalTime(request.getParameter("startTime"));
+        LocalTime endTime = parseLocalTime(request.getParameter("endTime"));
+        model.addAttribute("meals", super.getBetween(startDate, startTime, endDate, endTime));
+        return "meals";
     }
 
 }
--- src/main/webapp/WEB-INF/jsp/meals.jsp
+++ src/main/webapp/WEB-INF/jsp/meals.jsp
@@ -11,7 +11,7 @@
 <jsp:include page="fragments/bodyHeader.jsp"/>
 <section>
     <h3><h3><spring:message code="meal.title"/></h3></h3>
-    <form method="post" action="meals?action=filter">
+    <form method="post" action="meals">
         <dl>
             <dt><spring:message code="meal.fromDate"/></dt>
             <dd><input type="date" name="startDate" value="${param.startDate}"></dd>
@@ -31,7 +31,7 @@
         <button type="submit"><spring:message code="meal.filter"/></button>
     </form>
     <hr/>
-    <a href="meals?action=create"><spring:message code="meal.addMeal"/></a>
+    <a href="meals/create"><spring:message code="meal.addMeal"/></a>
     <hr/>
     <table border="1" cellpadding="8" cellspacing="0">
         <thead>
@@ -54,11 +54,12 @@
                 </td>
                 <td>${meal.description}</td>
                 <td>${meal.calories}</td>
-                <td><a href="meals?action=update&id=${meal.id}"><spring:message code="meal.update"/></a></td>
-                <td><a href="meals?action=delete&id=${meal.id}"><spring:message code="meal.delete"/></a></td>
+                <td><a href="meals/update?id=${meal.id}"><spring:message code="meal.update"/></a></td>
+                <td><a href="meals/delete?id=${meal.id}"><spring:message code="meal.delete"/></a></td>
             </tr>
         </c:forEach>
     </table>
 </section>
+<jsp:include page="fragments/footer.jsp"/>
 </body>
 </html>
